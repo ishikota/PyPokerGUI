9d2719acefee4ded7cb040ec2421949993556eec
Kota Ishimoto <ishikota086@gmail.com>
2017-03-23 15:20:32 +0900

    Refactor main script to run server

---
+++ pypokergui/server/poker.py
@@ -0,0 +1,134 @@
+import os
+import sys
+root = os.path.join(os.path.dirname(__file__), "../"*2)
+src_path = os.path.join(root, "pypokergui")
+sys.path.append(root)
+sys.path.append(src_path)
+
+import uuid
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+from tornado.options import define, options
+
+import pypokergui.server.game_manager as GM
+import pypokergui.server.message_manager as MM
+
+define("port", default=8888, help="run on the given port", type=int)
+define("config", default=None, help="path to game config", type=int)
+
+class Application(tornado.web.Application):
+
+    def __init__(self):
+        handlers = [
+                (r"/", PokerRequestHandler),
+                (r"/pokersocket", PokerWebSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, debug=True, **settings)
+
+class PokerRequestHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("index.html", config=global_game_config, registered=False)
+
+class PokerWebSocketHandler(tornado.websocket.WebSocketHandler):
+
+    sockets = set()
+
+    def get_compression_options(self):
+        # Non-None enables compression with default options.
+        return {}
+
+    def open(self):
+        self.uuid = str(uuid.uuid4())
+        PokerWebSocketHandler.sockets.add(self)
+
+    def on_close(self):
+        PokerWebSocketHandler.sockets.remove(self)
+        if global_game_config.get_human_player_info(self.uuid):
+            global_game_config.remove_human_player_info(self.uuid)
+            MM.broadcast_config_update(self, global_game_config, self.sockets)
+
+    def on_message(self, message):
+        js = tornado.escape.json_decode(message)
+        message_type = js['type']
+        if 'action_new_member' == message_type:
+            global_game_config.join_human_player(js['name'], self.uuid)
+            MM.broadcast_config_update(self, global_game_config, self.sockets)
+        elif 'action_start_game' == message_type:
+            if not global_game_config.is_playing_poker:
+                global_game_config.start_game()
+                MM.broadcast_start_game(self, global_game_config, self.sockets)
+                MM.broadcast_update_game(self, global_game_config, self.sockets)
+                if self._is_next_player_ai(global_game_config):
+                    self._progress_the_game_till_human()
+        elif 'action_declare_action' == message_type:
+            if self.uuid == global_game_config.next_player_uuid:
+                action, amount = js['action'], int(js['amount'])
+                global_game_config.update_game(action, amount)
+                MM.broadcast_update_game(self, global_game_config, self.sockets)
+                if self._is_next_player_ai(global_game_config):
+                    self._progress_the_game_till_human()
+        else:
+            raise Exception("Unexpected message [ %r ] received" % message)
+
+    def _progress_the_game_till_human(self):
+        while self._is_next_player_ai(global_game_config):  # TODO break if game has finished
+            if GM.has_game_finished(global_game_config.latest_messages): break
+            action, amount = global_game_config.ask_action_to_ai_player(
+                    global_game_config.next_player_uuid)
+            global_game_config.update_game(action, amount)
+            MM.broadcast_update_game(self, global_game_config, self.sockets)
+
+    def _is_next_player_ai(self, game_manager):
+        uuid = game_manager.next_player_uuid
+        return uuid and len(uuid) <= 2
+
+path = "/Users/kota/development/PyPokerGUIEnv/PyPokerGUI/pypokergui/ggg.py"
+global_game_config = GM.GameManager()
+sample_game_config = {
+        'max_round': 50,
+        'initial_stack': 10000,
+        'small_blind': 25,
+        'ante': 0,
+        'blind_structure': None,
+        'ai_players': [
+            { 'name': "ai1", "path": path },
+            { 'name': "ai2", "path": path },
+            { 'name': "ai3", "path": path },
+            { 'name': "ai4", "path": path },
+            { 'name': "ai5", "path": path },
+            { 'name': "ai6", "path": path },
+            { 'name': "ai7", "path": path },
+            { 'name': "ai8", "path": path },
+            { 'name': "ai9", "path": path },
+            { 'name': "ai10", "path": path },
+            ]
+        }
+
+def setup_config(config):
+    global_game_config.define_rule(
+            config['max_round'], config['initial_stack'], config['small_blind'],
+            config['ante'], config['blind_structure']
+    )
+    for player in config['ai_players']:
+        global_game_config.join_ai_player(player['name'], player['path'])
+
+def main():
+    tornado.options.parse_command_line()
+    config = sample_game_config  # TODO parse config file into hash
+    setup_config(config)
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    main()
+
